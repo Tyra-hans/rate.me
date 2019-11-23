1dbd15f3d7ec828bdb4da9c3710e1d7b06a1e4c2
Tyra-hans <tyrahans17@gmail.com>
2019-11-23 16:37:45 +0300

    Updates navbar to be dynamic depending on whether or not the user is authenticated

--- rateproject/urls.py
+++ rateproject/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-
 from rateapp import views
+from django.contrib.auth import views as auth_views
+from django.contrib.auth import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'' , include('rateapp.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
