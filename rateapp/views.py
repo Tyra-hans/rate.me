73db54618b336d4cb4bf2b805fbdebe1e81ec90f
Tyra-hans <tyrahans17@gmail.com>
2019-11-23 15:02:42 +0300

    Adds importation in the views.py from the models and restframe work . this is to allow us to access the models and serializers from the viewws so as to eventually display them

--- rateapp/views.py
+++ rateapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Person,Profile, Project , Rating 
+from .serializers import PersonSerializer ,ProjectSerializer, RatingSerializer
+
+
+
 # Create your views here.
