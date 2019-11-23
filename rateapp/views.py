f31de7c4e284cef4338ac93ab959a9a8c0454bd5
Tyra-hans <tyrahans17@gmail.com>
2019-11-23 15:49:54 +0300

    connects the project to the app in the settingsof the project, sets up registration for users to sign up

--- rateapp/views.py
+++ rateapp/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Person,Profile, Project , Rating 
-from .serializers import PersonSerializer ,ProjectSerializer, RatingSerializer
+from django.contrib.auth.models import User
+# from .models import Person,Profile, Project , Rating 
+# from .serializers import PersonSerializer ,ProjectSerializer, RatingSerializer
 
 
-
-# Create your views here.
+# def Home(request):
+#     return render(request,'registration/registration-form.html')
