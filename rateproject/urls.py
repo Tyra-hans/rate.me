e8d51872a6266a2b6915f29e88c3042c50b4e9f3
Tyra-hans <tyrahans17@gmail.com>
2019-11-23 14:55:14 +0300

    fixes file structure for the django application inorder to allow scalability and smooth deployment

---
+++ rateproject/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from rateapp import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'home/', views.Home.as_view()),
+]
