d927870c1bc4bd4e34c71f49dcb755b3972116d7
Tyra-hans <tyrahans17@gmail.com>
2019-11-25 14:57:07 +0300

    Creates Review form that allows user to rate the projects in terms of usability and other criteria

--- rateapp/views.py
+++ rateapp/views.py
@@ -7,8 +7,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.models import User
-from .models import Profile, Project  
-from .forms import UploadProjectForm , UpdateProfileForm
+from .models import Profile, Project , Review  
+from .forms import UploadProjectForm , UpdateProfileForm ,ReviewForm
 # from .serializers import PersonSerializer ,ProjectSerializer
 
 
@@ -36,6 +36,30 @@ def search(request):
         return render(request, 'all-posts/search.html', {'projects': projects, 'message': message})
         
     return render(request, 'all-posts/search.html')
+
+def project(request, id):
+    if request.user.is_authenticated:
+        user = User.objects.get(username = request.user)
+    project = Project.objects.get(id = id)
+    reviews = Review.objects.filter(project = project)
+    design = reviews.aggregate(Avg('design'))['design__avg']
+    usability = reviews.aggregate(Avg('usability'))['usability__avg']
+    content = reviews.aggregate(Avg('content'))['content__avg']
+    average = reviews.aggregate(Avg('average'))['average__avg']
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.average = (review.design + review.usability + review.content) / 3
+            review.project = project
+            review.user = user
+            review.save()
+        return redirect('project', id)
+    else:
+        form = ReviewForm()
+    return render(request, 'project.html', {'project': project, 'reviews': reviews, 'form': form, 'design': design, 'usability': usability, 'content': content, 'average': average})
+
+
 
 @login_required(login_url='/accounts/login/')
 def create_post(request):
